f8c47dfd2d73865a201bf322b2651d2d8b2a72cf
= <=>
2025-02-13 19:48:07 +0530

    Codechef snippet for fast results

---
+++ CODECHEF/Main.java
@@ -0,0 +1,74 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+ 
+    private static void solve(PrintWriter out, FastReader in){
+
+
+        
+
+
+
+
+
+
+
+
+
+    }
+    public static void main(String[] args) throws IOException {
+
+
+        PrintWriter out = new PrintWriter(System.out);
+        FastReader in = new FastReader();
+
+        solve(out,in);
+
+        out.flush(); 
+        out.close();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+        
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
